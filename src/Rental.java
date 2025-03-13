f48c500f1b2f1f309e0899625b60609a80a576a0
Pehan Januka <pehanjanu@gmail.com>
2025-03-14 02:13:18 +0530

    Created a getters for rental class

--- src/Rental.java
+++ src/Rental.java
@@ -7,6 +7,15 @@ public class Rental {
         this.car = car;
         this.customer = customer;
         this.days = days;
-
     }
+    public Car getCar() {
+        return car;
+    }
+    public Customer getCustomer(){
+        return customer;
+    }
+    public int getDays() {
+        return days;
+    }
+
 }
